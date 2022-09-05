0b8388b28f3222926edecb4ceb29cbb0296e1808
Martyn <zogacc@gmail.com>
2022-09-05 17:45:19 +0400

    feat: first public implementation

---
+++ src/bot.ts
@@ -0,0 +1,75 @@
+import { Context, Markup } from "telegraf";
+import axios from "axios"
+import { Answers, SS_URL, TELEGRAM_URL, WEB_APP_URL } from "./constants.js"
+import { addParticipantToSheet, checkUser, getNextChapter, setChapterAsRead } from "./spreadsheet.js"
+
+export async function onStart(ctx: Context) {
+  const username = ctx.message.from.username
+  const isLoggedIn = await checkUser(username)
+
+  if (isLoggedIn) {
+    ctx.reply('You are logged in')
+    
+    askChapter(ctx)
+  } else {
+    // TODO:  add reply with a keyboard
+    ctx.reply('Would you like to join a marathon? (WIP)', Markup.keyboard([
+      Markup.button.text(Answers.LOG_ME_IN),
+      Markup.button.text(Answers.NEVERMORE),
+    ]).oneTime().resize())
+  }
+}
+
+export async function logUserIn(ctx: Context) {
+  const username = ctx.message.from.username
+  const isLoggedIn = await checkUser(username)
+
+  if (isLoggedIn) {
+    ctx.reply('You are already logged in')
+  } else {
+    await addParticipantToSheet(username)
+    ctx.reply(`Now, you are logged in the marathon. \nYou can check it at ${SS_URL}`)
+  }
+}
+
+export async function onChapterYes(ctx: Context) {
+  console.log('onChapterYes')
+  const username = ctx.from.username
+  const nextChapter = await getNextChapter(username)
+
+  await setChapterAsRead(username, nextChapter)
+  ctx.reply('Ok, next chapter')
+  askChapter(ctx)
+}
+
+export async function askChapter(ctx: Context) {
+  const username = ctx.message.from.username
+  const nextChapter = await getNextChapter(username)
+
+  ctx.reply(
+    `Do you read chapter ${nextChapter}?`, 
+    Markup
+      .keyboard([
+        Markup.button.text(Answers.YES),
+        Markup.button.text(Answers.NO),
+        Markup.button.text(Answers.OTHER),
+      ])
+      .oneTime()
+      .resize()
+  )
+}
+
+export async function setWebhook ()  {
+  var url = TELEGRAM_URL + "/setWebhook?url=" + WEB_APP_URL;
+  await axios(url)
+}
+
+export async function test (ctx: Context) {
+  // console.log(ctx.message)
+  console.log("🚀 ~ test ~ TEST")
+  // ctx.callbackQuery.data = 'chapter_yes'
+}
+
+export async function selectOtherChapter(ctx: Context) {
+  console.log('selectOtherChapter WIP')
+}
---
+++ src/spreadsheet.ts
@@ -0,0 +1,105 @@
+import { google } from "googleapis"
+import { CREDENTIALS_PATH, SCOPES, SS_ID } from "./constants.js"
+
+const auth = new google.auth.JWT({
+  // TODO: add reading from secrets 
+  keyFile: CREDENTIALS_PATH,
+  scopes: SCOPES,
+})
+
+export async function getParticipantNameList(auth) {
+  const sheets = google.sheets({ version: 'v4', auth })
+
+  try {
+    return sheets.spreadsheets.values.get({
+      spreadsheetId: SS_ID,
+      range: 'Board!A2:A',
+    }).then(res => {
+      return res?.data?.values?.flat()
+    })
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export async function checkUser (username)  {
+  const users = await getParticipantNameList(auth)
+  return users?.includes(username)
+}
+
+export async function getNextChapter (username) {
+  const sheets = google.sheets({ version: 'v4', auth })
+  const userRowNumber = (await getUserRowIndex(username)) + 1
+  const range = `Board!B${userRowNumber}:${userRowNumber}`
+
+  const values = await sheets.spreadsheets.values.get({
+    spreadsheetId: SS_ID,
+    range,
+  }).then(res => {
+    return res?.data?.values?.flat()
+  })
+
+  const firstUnreadIndex = values?.indexOf('FALSE')
+  console.log("🚀 ~ getNextChapter ~ firstUnreadIndex", values, firstUnreadIndex)
+  return firstUnreadIndex && firstUnreadIndex !== -1 ? firstUnreadIndex + 1 : 1
+}
+
+export async function getUserRowIndex (username) {
+  const users = await getParticipantNameList(auth)
+
+  const userIndex = users?.indexOf(user => user === username)
+  if (!userIndex) {
+    return null
+  }
+
+  // +2 because of header and 0-based index
+  return userIndex + 2
+}
+
+export async function setChapterAsRead (username, chapterIndex) {
+  const sheets = google.sheets({ version: 'v4', auth })
+  const userRowIndex = (await getUserRowIndex(username)) + 1
+  const chapterAlphabetIndex = String.fromCharCode(chapterIndex + 1 + 64)
+
+  const range = `Board!${chapterAlphabetIndex}${userRowIndex}:AO${userRowIndex}`
+  const requestBody = { values: [[true]] }
+  // console.log("🚀 ~ setChapterAsRead ~ range", range)
+
+  try {
+    const response = await sheets.spreadsheets.values.update({
+      spreadsheetId: SS_ID,
+      requestBody,
+      valueInputOption: 'USER_ENTERED',
+      range,
+    })
+    // console.log("🚀 ~ setChapterAsRead ~ response", response)
+
+    return response
+  } catch (err) {
+    console.error(err) 
+  }
+}
+
+export async function addParticipantToSheet (username) {
+  const sheets = google.sheets({ version: 'v4', auth })
+  const userRowNumber = (await getUserRowIndex(auth)) + 1
+  const range = `Board!A${userRowNumber}`
+  // const userCell = { userEnteredValue: username, hyperlink: `https://t.me/${username}` }
+
+  const userRow = [`=hyperlink("https://t.me/${username}"; "@${username}")`]
+  // TODO: rework to adding new row
+  const requestBody = { values: [userRow] }
+
+  try {
+    const response = await sheets.spreadsheets.values.append({
+      valueInputOption: 'USER_ENTERED',
+      spreadsheetId: SS_ID,
+      range,
+      requestBody,
+    })
+
+    return response
+  } catch (err) {
+    console.error(err) 
+  }
+}
